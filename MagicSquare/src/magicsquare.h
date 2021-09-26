4513240f4b7ba6b9e595ebe33286f623efffd45a
Takeuchi-Akihide <htasn1110@gmail.com>
2021-09-26 13:52:01 +0900

    魔法陣プログラムを作成

---
+++ MagicSquare/src/magicsquare.h
@@ -0,0 +1,14 @@
+/* magicsquare.h
+ *  Created on: 2021/09/25
+ *      Author: a_takeuchi
+ */
+
+#ifndef MAGICSQUARE_H_
+#define MAGICSQUARE_H_
+
+#define N   9
+
+void make_square(void);
+void print_square(void);
+
+#endif /* MAGICSQUARE_H_ */
