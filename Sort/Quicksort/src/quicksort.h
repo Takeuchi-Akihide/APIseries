e11e80d2157e1cbdfeb9260f53cdf534fefa1cfb
Takeuchi-Akihide <htasn1110@gmail.com>
2021-10-08 22:12:47 +0900

    C言語でクイックソートを実装

---
+++ Sort/Quicksort/src/quicksort.h
@@ -0,0 +1,15 @@
+/* magicsquare.h
+ *  Created on: 2021/09/25
+ *      Author: a_takeuchi
+ */
+
+#ifndef QUICKSORT_H_
+#define QUICKSORT_H_
+
+int getDigit(int num);
+void print_by_10(int *data, int hnum);
+void make_data_random(int *data, int hnum);
+void quick_sort(int *data, int hnum);
+// void quick_sort(int *data, int *res, int hnum);
+
+#endif /* QUICKSORT_H_ */
